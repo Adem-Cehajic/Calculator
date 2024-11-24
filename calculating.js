de5963ba413fd501094bc419d631e1a48055da6c
Adem Cehajic <ademc1191@gmail.com>
2024-11-23 22:36:04 -0500

    added more buttons
    I added a decimal and bracket buttons so more math operations are possible I also put in a clear and answer button for convenience

--- calculating.js
+++ calculating.js
@@ -1,13 +1,24 @@
 const buts = document.querySelectorAll("button");
 const screen = document.querySelector(".top");
 const ans = document.querySelector(".bottom");
+let answer = 0;
 buts.forEach((button) => {
     button.addEventListener("click", () => {
     if(button.className == "equal") {
+        answer = eval(String(screen.innerHTML));
         ans.innerHTML = String(screen.innerHTML) + '=' + eval(String(screen.innerHTML));
-        screen.innerHTML = '';
     }
     else {
-    screen.innerHTML = screen.innerHTML + String(button.innerHTML);
+        if(button.className == "clear"){
+            screen.innerHTML = '';
+        }
+        else{
+            if(button.className == "answer"){
+                screen.innerHTML = screen.innerHTML + String(answer);
+            }
+            else {
+                screen.innerHTML = screen.innerHTML + String(button.innerHTML);
+            }
+        }
     }})
 });
